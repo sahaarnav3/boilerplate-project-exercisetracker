af97b57be64230ed10368da37a79b7c63a03cf3c
Arnav Saha <sahaarnav3@gmail.com>
2024-05-02 19:09:57 +0530

    Major changes, there are 2 seperate collections now, user and exercises. The working has been changed accordingly..

--- index.js
+++ index.js
@@ -29,19 +29,25 @@ const userSchema = new mongoose.Schema({
     type: String,
     unique: true,
     required: true
-  },
-  count: Number,
-  log: [{
-    description: String,
-    duration: Number,
-    date: String
-  }],
+  }
 });
 let User = mongoose.model('User', userSchema);
 
+//The Schema for exercise logs--
+const exerciseSchema = mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true
+  },
+  description: String,
+  duration: Number,
+  date: Date
+})
+let Exercise = mongoose.model('Exercise', exerciseSchema);
+
 //To create and save the user in database -- 
 const createAndSaveUser = async (userName) => {
-  let userOne = new User({ username: userName, count: 0, log: [] });
+  let userOne = new User({ username: userName });
   try {
     const savedUser = await userOne.save();
     return savedUser;
@@ -74,62 +80,116 @@ app.get("/api/users", async (req, res) => {
   res.send(allUsers);
 })
 
+//to convert date to the string format asked in question..
+const toDateString = (value) => {
+  value = new Date(value);
+  let dateArray = value.toLocaleDateString('en-US', {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  }).replaceAll(",", "").split(" ");
+  if (parseInt(dateArray[2]) < 10) {
+    dateArray[2] = "0" + dateArray[2];
+  }
+  return dateArray.join(" ");
+}
+
+
 //Logic for saving the exercise according to user id generated in first step...
 app.post("/api/users/:_id/exercises", async (req, res) => {
   const userId = req.params._id;
-  let fetchedUser = "";
+  let exercise = "";
+  let fetchedUser = ""
+  let { description, duration, date } = req.body;
   try {
     fetchedUser = await User.findById(userId);
-
-    //Date Manipulation
-    let dateString = req.body.date;
-    if (!dateString)
-      dateString = new Date();
-    else
-      dateString = new Date(Date.parse(dateString));
-    let dateArray = dateString.toLocaleDateString('en-US', {
-      weekday: 'short',
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric'
-    }).replaceAll(",", "").split(" ");
-    if (parseInt(dateArray[2]) < 10) {
-      dateArray[2] = "0" + dateArray[2];
+    if (!fetchedUser) {
+      res.json({ "Error": "User_Id doesn't exist. Please enter correct Id." })
+      return;
     }
-    req.body.date = dateArray.join(" ");
-
-    fetchedUser.log.push(req.body);
-    fetchedUser.count = fetchedUser.count + 1;
-    fetchedUser = await fetchedUser.save();
-    // console.log(fetchedUser);
+    duration = parseInt(duration);
+    date = date ? new Date(date) : new Date()
+    const exerciseObj = new Exercise({
+      user_id: userId,
+      description: description,
+      duration: duration,
+      date: date
+    });
+    console.log(exerciseObj);
+    exercise = await exerciseObj.save();
   } catch (error) {
     console.log(error);
-    res.json({ "Error": "Some error Occured" });
+    res.json({ "Error": "Someother issue occured, try again." });
     return;
   }
   res.json({
-    "_id": userId, "username": fetchedUser.username, "date": req.body.date, "duration": req.body.duration,
-    "description": req.body.description
+    "_id": userId, "username": fetchedUser.username, "date": toDateString(date),
+    "duration": duration, "description": description
   });
 });
+
+//Below function will be used to fetch the logs from DB in accordance with the query parameters..
+const fetchUsersWithParams = async (userId, fromDate, toDate, limit) => {
+  let fetchedUser = "";
+  let fetchedExercises = "";
+  let responseToReturn = {};
+  console.log(fromDate, toDate, limit);
+  try {
+    fetchedUser = await User.findById(userId);
+
+    let dateObj = {};
+    if (fromDate) {
+      dateObj["$gte"] = new Date(fromDate);
+    }
+    if (toDate) {
+      dateObj["$lte"] = new Date(toDate);
+    }
+    let filter = {
+      user_id: userId
+    }
+    if (fromDate || toDate) {
+      filter.date = dateObj;
+    }
+    console.log(filter);
+    fetchedExercises = await Exercise.find(filter).limit(+limit || 1000); // + is used to parse the string to int.
+
+    //Fabricating exercise logs according to question--
+    responseToReturn.id = userId;
+    responseToReturn.username = fetchedUser.username;
+    responseToReturn.count = fetchedExercises.length;
+    responseToReturn.logs = [];
+    fetchedExercises.forEach(elem => {
+      responseToReturn.logs.push({
+        "description" : elem.description,
+        "duration": elem.duration,
+        "date": toDateString(elem.date)
+      })
+    })
+    return responseToReturn;
+  }
+  catch (error) {
+    console.log(error);
+    return;
+  }
+}
 
 //Below router will be used to get exercise logs of a user with the help of "_id"
 app.get("/api/users/:_id/logs", async (req, res) => {
   const userId = req.params._id;
-  let fetchedUser = "";
-  try {
-    fetchedUser = await User.findById(userId);
-  } catch (error) {
+  const fromDate = req.query.from || null;
+  const toDate = req.query.to || null;
+  const limit = req.query.limit || null;
+  let fetchedUser = await fetchUsersWithParams(userId, fromDate, toDate, limit);
+  if (!fetchedUser) {
     res.json({ "error": "Either _id wrong or some other issue caused, try again." });
+    return;
   }
   res.json({
-    "_id": fetchedUser._id, "username": fetchedUser.username, "count": fetchedUser.count,
-    "log": fetchedUser.log
+    "_id": fetchedUser.id, "username": fetchedUser.username, "count": fetchedUser.count,
+    "log": fetchedUser.logs
   });
 });
-
-
-
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
